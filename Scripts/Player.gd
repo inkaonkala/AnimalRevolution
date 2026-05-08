643cac65e0ad9d49b678e49b4f77082b0d763aad
Inka <inka@Inkas-MacBook-Pro.local>
2026-05-08 14:14:25 +0300

    human attack and capture

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 
 @export var speed := 200.0
+@onready var attack_area = $AttackArea
 
 var can_move := true
 
+var attack_power := 1
+var hp := 10
+var enemies_in_attack_area: Array = []
+
+func _ready() -> void:
+	add_to_group("player")
+	attack_area.body_entered.connect(attackarea_entered)
+	attack_area.body_exited.connect(attackarea_exited)
+
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		attack()
 	
 	if not can_move:
 		velocity = Vector2.ZERO
@@ -20,3 +32,35 @@ func _physics_process(delta: float) -> void:
 
 	velocity = input_direction * speed
 	move_and_slide()
+	
+func take_damage(amount: int) -> void:
+	hp -= amount
+	print("Player hp:", hp)
+
+	if hp <= 0:
+		print("PlayerDefeted!")
+
+func attackarea_entered(body: Node) -> void:
+	if body == self:
+		return
+		
+	if body.has_method("take_damage"):
+		print("Can damage: ", body.name)
+		enemies_in_attack_area.append(body)
+
+func attackarea_exited(body: Node) -> void:
+	if body in enemies_in_attack_area:
+		enemies_in_attack_area.erase(body)
+
+func attack() -> void:
+	print("Player attacks!")
+
+	if enemies_in_attack_area.size() <= 0:
+		print("No ENEMY!")
+		return
+
+	var target = enemies_in_attack_area[0]
+	print("There's an enemy! ")
+
+	if target.has_method("take_damage"):
+		target.take_damage(attack_power)
--- human.gd
+++ human.gd
@@ -5,12 +5,14 @@ enum State
 	ROAMING,
 	SLEEPING,
 	DEFEDING,
-	CAPTURED
+	CAPTURED,
+	TAGGED
 }
 
 @export var normal_tex: Texture2D
 @export var sleeping_tex: Texture2D
 @export var captured_tex: Texture2D
+@export var tagged_tex: Texture2D
 
 @export var speed := 35.0
 @export var hp := 5
@@ -21,20 +23,37 @@ enum State
 @onready var interact_area = $InteractArea
 
 var captured := false
+var attacked := false
+var tagged := false
+var player_near := false
+
 var state := State.ROAMING
 var move_dir := Vector2.ZERO
 var change_dir_timer := 0.0
 var player_in_attack_area := false
+var attack_cooldown := 0.0
+
 var player = null
 
 func _ready() -> void:
 	randomize()
+	attack_area.body_entered.connect(attackarea_entered)
+	attack_area.body_exited.connect(attackarea_exited)
+	interact_area.body_entered.connect(interact_entered)
+	interact_area.body_exited.connect(interact_exited)
 	choose_new_dir()
 	update_by_time()
+
+func _process(_delta: float) -> void:
+	if captured and player_near and Input.is_action_just_pressed("interact"):
+		tag_for_factory()
 	
 func _physics_process(delta: float) -> void:
 	if captured:
 		return
+		
+	if attack_cooldown > 0:
+		attack_cooldown -= delta
 	
 	update_by_time()
 	
@@ -44,11 +63,24 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
+	
+func attackarea_entered(body: Node)-> void:
+	if body.is_in_group("player"):
+		player_in_attack_area = true
+		player = body
+		
+func attackarea_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_in_attack_area = false
+		player = null
 		
 func update_by_time() -> void:
 	#check what ToD and make sleep, now just roam
 	if captured:
 		state = State.CAPTURED
+	elif attacked and player_in_attack_area and player != null:
+		attack_player()
+		return	
 	else:
 		state = State.ROAMING
 		
@@ -77,16 +109,29 @@ func take_damage(amount: int) -> void:
 	if captured:
 		return
 	hp -= amount
+	attacked = true
 	print("Human hp: ", hp)
-		
+	
 	if hp <= 0:
 		become_captured()
 	else:
 		defend()
+		
 			
 func defend() -> void:
 	state = State.DEFEDING
 	print("Human defends!")
+	
+func attack_player() -> void:
+	if attack_cooldown > 0:
+		return
+	state =  State.DEFEDING
+	velocity = Vector2.ZERO
+	print("HUMAN attacks player!!")
+	
+	if player.has_method("take_damage"):
+		player.take_damage(attack_power)
+	attack_cooldown = 1.0
 			
 func become_captured() -> void:
 	captured = true
@@ -94,12 +139,32 @@ func become_captured() -> void:
 	velocity = Vector2.ZERO
 	update_image()
 	print("Human captured!")
+	
+func interact_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_near = true
+
+func interact_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_near = false
+		
+func tag_for_factory() -> void:
+	if tagged:
+		return
+
+	tagged = true
+	state = State.TAGGED
+	update_image()
+	
+	print("Human tagged for factory!")
 		
 func update_image() -> void:
 	if state == State.CAPTURED and captured_tex:
 		sprite.texture = captured_tex
 	elif state == State.SLEEPING and sleeping_tex:
 		sprite.texture = sleeping_tex
+	elif state == State.TAGGED and tagged_tex:
+		sprite.texture = tagged_tex
 	elif normal_tex:
 		sprite.texture = normal_tex
 			
