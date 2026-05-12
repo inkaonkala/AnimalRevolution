e905ab582fc0d1b370af0c11003b712c7bc4d92e
Inka <inka@Inkas-MacBook-Pro.local>
2026-05-12 10:00:26 +0300

    added script to Cat. Added turtle to GDD

---
+++ Scripts/cat.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+@onready var speak_bubble = $Label
+
+enum State
+{
+	LOST,
+	HAPPY,
+	SAD,
+	WORKING,
+	HUNGRY,
+	NEEDS_THERAPY
+}
+
+var state := State.LOST
+
+var work_nights := 0
+var hunger_timer := 0
+var humans_in_factory := false
+var meat_produced := 0
+
+func on_new_day() -> void:
+	if state == State.HAPPY:
+		hunger_timer += 1
+	if hunger_timer >= 3:
+		become_hungry()
+		
+	elif state == State.WORKING:
+		butcher()
+		work_nights += 1
+		
+		if work_nights >= 3:
+			need_therapy()
+
+func become_hungry() ->void:
+	print("Cat's hungry!")
+	
+func butcher() -> void:
+	print("Cat's butshering the human")
+
+func need_therapy() -> void:
+	print("Cat's in need of threrapy")
