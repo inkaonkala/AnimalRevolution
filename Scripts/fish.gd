3df49019585146cfd9f98c5d5a73f7ac3549b961
inkaonkala <inkaisinka@live.com>
2026-07-14 13:33:59 +0300

    fish updated

--- Scripts/fish.gd
+++ Scripts/fish.gd
@@ -10,6 +10,11 @@ enum State {
 
 @export var fish_name := "Bubsy"
 @export var spawn_index := 0
+@export var aquarium_tex: Texture2D
+@export var fish_text: Texture2D
+
+@onready var minigame_dialog: ConfirmationDialog = $MiniGameDialog
+@onready var fish_sprite: Sprite2D = $Sprite2D
 
 var state := State.LOST
 var has_joined := false
@@ -24,12 +29,36 @@ func _ready() -> void:
 		
 	super._ready()
 	DayCycle.new_day.connect(on_new_day)
-		
+	minigame_dialog.confirmed.connect(minigame_triggered)
+	
+	if aquarium_tex != null:
+		fish_sprite.texture = aquarium_tex
+
+func pipe_check() -> bool:
+	var main := get_tree().current_scene
+	if main == null:
+		return false
+	if not ("inventory" in main):
+		push_warning("Fish game could not find inventory")
+		return false
+	return main.inventory.get("pipe", 0) >= 10
 
 func first_meeting() -> void:
-	await super.first_meeting()
-	state = State.NEUTRAL
-	emit_emotion_changed()
+	#await super.first_meeting()
+	if has_joined:
+		await talk()
+		return
+	if not pipe_check():
+		await say_this("With 10 pipes, I could help you and our fish community!")
+		return
+		
+	await say_this("Ypu have enough pipes! Want to help FISH?")
+	
+	minigame_dialog.dialog_text = \
+		"Use 10 pipes to play the build_aquarium_game?"
+	minigame_dialog.popup_centered()
+	#state = State.NEUTRAL
+	#emit_emotion_changed()
 	
 func talk() -> void:
 	match state:
@@ -54,3 +83,58 @@ func get_emotion_value() -> int:
 			
 func is_unlocked() -> bool:
 	return state != State.LOST
+	
+func minigame_triggered() -> void:
+	if has_joined:
+		return
+	if not pipe_check():
+		await say_this("Your pipes seemed to have dissapeared")
+		return
+	has_triggered_minigame = true
+	
+	print("Minigame would happen here!")
+	
+	var temp_minigameresult := true
+	complete_minigame(temp_minigameresult)
+	
+func complete_minigame(was_success: bool) -> void:
+	has_triggered_minigame = false
+	
+	if not was_success:
+		await say_this("Blub! Let's try again with more pipes!")
+		return
+	if not pipe_check():
+		await say_this("We need more pipes!")
+		return
+	use_pipes()
+	
+	has_joined = true
+	state = State.HAPPY
+	move_to_basement()
+	
+	if fish_text != null:
+		fish_sprite.texture = fish_text
+	emit_emotion_changed()
+	await say_this("I'll move to basement tank now!")
+
+func use_pipes() -> void:
+	var main := get_tree().current_scene
+	if main == null:
+		return
+	if main.has_method("remove_item"):
+		main.remove_item("pipe", 10)
+	else:
+		push_warning("Function not found in main: remove_item")
+
+func move_to_basement() -> void:
+	var main := get_tree().current_scene
+	
+	if main == null:
+		return
+	
+	var basement := main.get_node_or_null("basement")
+	if basement == null:
+		push_warning("BASEMENT could not be found")
+		return
+	
+	#ADD PSAWN LOGIC HERE!
